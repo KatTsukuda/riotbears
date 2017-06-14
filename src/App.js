eb9d12abc0800803f23cc47407ee06f7e240d913
kat tsukuda <bearheartsbat@gmail.com>
2017-06-14 15:10:25 -0700

    initial commit and setup of header and nav

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h2>Riot Bears</h2>
+        <ul className="Nav">
+          <li>Pies</li>
+          <li>Activism</li>
+          <li>Allies</li>
+        </ul>
+        <div className="Content">
+        STUFF
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
